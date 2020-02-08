e2d6e346ac36a7271ed8faa49021171cf113aced
Noam Sauer-Utley <noamsauerutley@gmail.com>
2020-02-08 17:22:19 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function findAllPermutations(string){
+  if (string.length < 2){
+    return string
+  }
+
+  let permutationsArray = [] 
+  
+  for (let i = 0; i < string.length; i++){
+    let char = string[i]
+
+    if (string.indexOf(char) != i)
+    continue
+
+    let remainder = string.slice(0, i) + string.slice(i + 1, string.length)
+
+    for (let permutation of findAllPermutations(remainder)){
+      permutationsArray.push(char + permutation) }
+  }
+  return permutationsArray
+}
